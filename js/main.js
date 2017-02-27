2e36fe2c6fa004a0e3df3a497bf6908026647e87
Harry J Burgess <hburgess.dev@gmail.com>
2017-02-27 23:05:34 +0000

    Tidy code and position relative on sub nav

--- js/main.js
+++ js/main.js
@@ -1,15 +1,8 @@
-// Nav dropdown animation
-$(document).ready(function () {
-    $(".menu-text").click(function () {
-        $(".top-layer-nav-list").slideToggle(200);
-        $('#top-nav > ul li ul').hide();
-    });
-    // This animated the toggle.
-    if ($('.target').is(':visible')) {}
+$(".menu-text").click(function () {
+	$(".top-layer-nav-list").slideToggle(200);
+    $('#top-nav > ul li ul').hide();
 });
-$('#top-nav li ul').hide().removeClass('fallback');
 $('#top-nav > .top-layer-nav-list > li').click(function () {
     $('#top-nav li ul').not($(this).children("ul").slideToggle(200)).hide();
     $('#top-nav li').not($(this).toggleClass('extended')).removeClass('extended');
 });
-
